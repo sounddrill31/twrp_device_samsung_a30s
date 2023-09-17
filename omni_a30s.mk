897fbe25e28b7984efe7a4d47c27b42166f78de7
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-09-17 18:09:52 +0530

    Update omni_a30s.mk

--- omni_a30s.mk
+++ omni_a30s.mk
@@ -21,7 +21,7 @@ PRODUCT_RELEASE_NAME := a30s
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk) 
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a30s
